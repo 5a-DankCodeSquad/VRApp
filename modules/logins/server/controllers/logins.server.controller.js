d8776b60da236bdb2b310086a49834c2c55437e9
Nick Topper <lwapd@Nicks-MacBook-Air.local>
2015-10-08 11:08:11 -0400

    whitespace

--- modules/logins/server/controllers/logins.server.controller.js
+++ modules/logins/server/controllers/logins.server.controller.js
@@ -10,42 +10,42 @@ var config = require(appDir + '/config/config');
 /**
  * authenticate and get a new sessionId
  */
-exports.getSession = function (req, res) {
+exports.getSession = function(req, res) {
 
-  //make request to offr.com endpoint
-	var request = require("request");
+    //make request to offr.com endpoint
+    var request = require("request");
 
-	request.get(
-  		config.offrApiUri + 'getUser.cfm' +
-  			"?Email=" + req.body.user +
-  			"&Password=" + req.body.password +
-  			"&APIKEY=" + config.offrApiKey,
+    request.get(
+        config.offrApiUri + 'getUser.cfm' +
+        "?Email=" + req.body.user +
+        "&Password=" + req.body.password +
+        "&APIKEY=" + config.offrApiKey,
 
-	 function(error, response, body) {
+        function(error, response, body) {
 
-        if (error) {
-        	res.status(500).send(error);
-        } else {
+            if (error) {
+                res.status(500).send(error);
+            } else {
 
-        	//get userID from parsed responce body
-        	var parsed = JSON.parse(body);
+                //get userID from parsed responce body
+                var parsed = JSON.parse(body);
 
-          //DATA is empty when auth fails
-          if (parsed.DATA.length > 0) {
+                //DATA is empty when auth fails
+                if (parsed.DATA.length > 0) {
 
-            var index = parsed.COLUMNS.indexOf("USERID");
-            var userId = parsed.DATA[0][index];
+                    var index = parsed.COLUMNS.indexOf("USERID");
+                    var userId = parsed.DATA[0][index];
 
-            //make a new session
-            req.session.userId = userId;
+                    //make a new session
+                    req.session.userId = userId;
 
-            //now they never need to login again
-            res.send();
+                    //now they never need to login again
+                    res.send();
 
-          } else {
-              //invalid login
-              res.status(403).send("invalid username or password");
-          }
-		}
-	});
+                } else {
+                    //invalid login
+                    res.status(403).send("invalid username or password");
+                }
+            }
+        });
 };
