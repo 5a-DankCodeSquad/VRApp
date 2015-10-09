f5533a8cb927c4e83ba4437de67b46e8ac22bf69
Nick Topper <lwapd@Nicks-MacBook-Air.local>
2015-10-09 18:03:01 -0400

    added controller for report data

--- modules/reports/server/controllers/reports.server.controller.js
+++ modules/reports/server/controllers/reports.server.controller.js
@@ -1,96 +1,27 @@
 'use strict';
 
 /**
- * Module dependencies.
+ * test report data
  */
-var _ = require('lodash'),
-	path = require('path'),
-	mongoose = require('mongoose'),
-	Report = mongoose.model('Report'),
-	errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
-
-/**
- * Create a Report
- */
-exports.create = function(req, res) {
-	var report = new Report(req.body);
-	report.user = req.user;
-
-	report.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
+var reportTest = function() {
+    return "{'foo':5,'bar':7}";
 };
 
-/**
- * Show the current Report
- */
-exports.read = function(req, res) {
-	res.jsonp(req.report);
-};
 
 /**
- * Update a Report
+ * authenticate and get a new sessionId
  */
-exports.update = function(req, res) {
-	var report = req.report ;
+exports.getReportData = function (req, res) {
 
-	report = _.extend(report , req.body);
+    //to hold JSON string
+    var data = '';
 
-	report.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
-};
-
-/**
- * Delete an Report
- */
-exports.delete = function(req, res) {
-	var report = req.report ;
-
-	report.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(report);
-		}
-	});
-};
-
-/**
- * List of Reports
- */
-exports.list = function(req, res) { Report.find().sort('-created').populate('user', 'displayName').exec(function(err, reports) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(reports);
-		}
-	});
-};
-
-/**
- * Report middleware
- */
-exports.reportByID = function(req, res, next, id) { Report.findById(id).populate('user', 'displayName').exec(function(err, report) {
-		if (err) return next(err);
-		if (! report) return next(new Error('Failed to load Report ' + id));
-		req.report = report ;
-		next();
-	});
+    //select the right data
+	switch(req.query.report_type) {
+			case "test":
+				data = reportTest();
+				break;
+	}
+ 
+    res.send(data);
 };
--- modules/reports/server/routes/reports.server.routes.js
+++ modules/reports/server/routes/reports.server.routes.js
@@ -2,18 +2,8 @@
 
 module.exports = function(app) {
 	var reports = require('../controllers/reports.server.controller');
-	var reportsPolicy = require('../policies/reports.server.policy');
 
 	// Reports Routes
 	app.route('/api/reports').all()
-		.get(reports.list).all(reportsPolicy.isAllowed)
-		.post(reports.create);
-
-	app.route('/api/reports/:reportId').all(reportsPolicy.isAllowed)
-		.get(reports.read)
-		.put(reports.update)
-		.delete(reports.delete);
-
-	// Finish by binding the Report middleware
-	app.param('reportId', reports.reportByID);
+		.get(reports.getReportData);
 };
