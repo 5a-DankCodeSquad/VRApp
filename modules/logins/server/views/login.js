42a426c497aa9c0ef50b15f07f0619332b739212
Francis Perez <fcsp19191993@ufl.edu>
2015-10-16 13:38:15 -0400

    This is for Mathhhhhew

---
+++ modules/logins/server/views/login.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var myLogin = angular.module('app', []);
+
+myLogin.controller('loginCtrl',function($scope) {
+	$scope.isUp = false;
+	$scope.signUp = function() {
+		$scope.isUp = true;
+	};
+
+	$scope.isSignUp = function() {
+		if($scope.isUp === true)
+		{
+			return $scope.isUp;
+		}
+	};
+
+	$scope.signIn = function($scope, $http) {
+		$scope.isUp = false;
+		$http.post;
+	};
+});
