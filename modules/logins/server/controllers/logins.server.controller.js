dfe60b900eb80c12bc454abbb80255f2a8d3961c
Joe Nguyen <joenguyen@Joes-MacBook-Pro.local>
2015-10-31 14:39:14 -0400

    commited login page

--- modules/logins/server/controllers/logins.server.controller.js
+++ modules/logins/server/controllers/logins.server.controller.js
@@ -5,34 +5,47 @@ var path = require('path');
 var appDir = path.dirname(require.main.filename);
 var config = require(appDir + '/config/config');
 
+
+
 /**
  * authenticate and get a new sessionId
  */
-exports.getSession = function (req, res) {
+exports.getSession = function(req, res) {
 
-  //make request to offr.com endpoint
-	var request = require("request");
+    //make request to offr.com endpoint
+    var request = require("request");
 
-	request.get(
-  		config.offrApiUri + 'getUser.cfm' +
-  			"?Email=" + req.body.user +
-  			"&Password=" + req.body.password +
-  			"&APIKEY=" + config.offrApiKey,
+    request.get(
+        config.offrApiUri + 'getUser.cfm' +
+        "?Email=" + req.body.user +
+        "&Password=" + req.body.password +
+        "&APIKEY=" + config.offrApiKey,
 
-	 function(error, response, body) {
-        if (error) {
-        	res.status(500).send(error);
-        } else {
-        	//get userID from parsed responce body
-        	var parsed = JSON.parse(body);
-			    var index = parsed.COLUMNS.indexOf("USERID");
-			    var userId = parsed.DATA[0][index];
+        function(error, response, body) {
 
-			    //make a new session
-          req.session.userId = userId;
+            if (error) {
+                res.status(500).send(error);
+            } else {
 
-			    //now they never need to login again
-          res.send(); 
-		}
-	});
+                //get userID from parsed responce body
+                var parsed = JSON.parse(body);
+
+                //DATA is empty when auth fails
+                if (parsed.DATA.length > 0) {
+
+                    var index = parsed.COLUMNS.indexOf("USERID");
+                    var userId = parsed.DATA[0][index];
+
+                    //make a new session
+                    req.session.userId = userId;
+
+                    //now they never need to login again
+                    res.send();
+
+                } else {
+                    //invalid login
+                    res.status(403).send("invalid username or password");
+                }
+            }
+        });
 };
