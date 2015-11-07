051d3c223e4fdaac4375eb6caa5c2149a5ba8970
Francis Perez <fcsp19191993@ufl.edu>
2015-11-07 18:07:14 -0500

    Sorry again, Nick

--- public/app/js/table/delete_row.js
+++ public/app/js/table/delete_row.js
@@ -1,6 +1,7 @@
 $(".deleterow").on("click", function(){
-    var $killrow = $(this).parent('tr');
-    $killrow.addClass("danger");
-    $killrow.fadeOut(500, function(){
-        $(this).remove();
-    });});
+                   var $killrow = $(this).parent('tr');
+                   $killrow.addClass("danger");
+                   $killrow.fadeOut(500, function(){
+                   $(this).remove();
+             });});
+        
