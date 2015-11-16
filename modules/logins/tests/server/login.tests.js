48107ee56b760751b666629925ebb9376bcb6bb2
Leslie Richardson <bookworm42795@gmail.com>
2015-11-16 18:24:54 -0500

    grunt won't work right for some reason, so I cant properly test with protractor yet :(

---
+++ modules/logins/tests/server/login.tests.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Reports E2E Tests:', function() {
+	describe('Test Reports page', function() {
+		/*it('Should not include new Reports', function() {
+			browser.get('http://localhost:3000/#!/reports');
+			expect(element.all(by.repeater('report in reports')).count()).toEqual(0);
+		});*/
+		it('Valid login', function() {
+			browser.get('http://localhost:3000/login');
+			var username = element(by.id('inputEmail'));
+			username.sendKeys("agent@demo.com");
+
+			var password = element(by.id('inputPassword'));
+			password.sendKeys("demo");
+
+			var button = element(by.buttonText('Sign In'));
+			button.click();
+
+			expect(browser.getCurrentUrl()).toMatch('localhost:3000/app/');
+		});
+	});
+});
