e10b8fc2ab41db4c7551343cc7bd3e5896e9fbb4
Leslie Richardson <bookworm42795@gmail.com>
2015-11-18 12:25:07 -0500

    protractor tests successful for valid/invalid login tests

--- modules/logins/tests/server/login.tests.js
+++ modules/logins/tests/server/login.tests.js
@@ -19,5 +19,18 @@ describe('Reports E2E Tests:', function() {
 
 			expect(browser.getCurrentUrl()).toMatch('localhost:3000/app/');
 		});
+		it('invalid login', function() {
+			browser.get('http://localhost:3000/login');
+			var username = element(by.id('inputEmail'));
+			username.sendKeys("agt@demo.com");
+
+			var password = element(by.id('inputPassword'));
+			password.sendKeys("demo");
+
+			var button = element(by.buttonText('Sign In'));
+			button.click();
+
+			expect(browser.getCurrentUrl()).toMatch('localhost:3000/login');
+		});
 	});
 });
