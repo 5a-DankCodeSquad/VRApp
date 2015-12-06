e34efc6544caa5e98d1e95eca06294d328cde7cc
ntopper <nicktopper@gmail.com>
2015-12-05 23:19:35 -0500

    unfucked everything again like usual

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -8,53 +8,14 @@ app.controller('main', function($scope) {
 /* Overview Controller */
 app.controller('overview', function($scope, $http) { 
     $scope.report = []; 
-    $scope.keys = ["Clicks", "Impressions", "CTR", "CPM", "Budget USD", "Cost USD", "Start Date", "End Date", "Created Date"];
+    $scope.keys = ["clicks", "impressions", "ctr", "cpm", "budget_USD", "cost_USD", "start_date", "end_date", "created_date"];
     $scope.overview_ready = false;
     
     $http.get('/api/reports?report_type=campaign').then(
         function(response) { 
-        	console.log(response.data.results[0]);
-            //Adding necesary data and checking if null
-            if(response.data.results[0].clicks != null)
-                $scope.report.push(response.data.results[0].clicks);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].impressions != null)
-                $scope.report.push(response.data.results[0].impressions);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].ctr != null)
-                $scope.report.push(response.data.results[0].ctr);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].cpm != null)
-                $scope.report.push(response.data.results[0].cpm);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].budget_USD != null)
-                $scope.report.push(response.data.results[0].budget_USD);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].cost != null)
-                $scope.report.push(response.data.results[0].cost);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].start_date != null)
-                $scope.report.push(response.data.results[0].start_date);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].end_date != null)
-            $scope.report.push(response.data.results[0].end_date);
-            else
-                $scope.report.push("N/A");
-            if(response.data.results[0].created_date != null)
-                $scope.report.push(response.data.results[0].created_date);
-            else
-                $scope.report.push("N/A");
-
-            //$scope.report = response.data.results[0];
+            $scope.report = response.data.results[0];
             $scope.overview_ready = true;
-        }, 
+        },
         function(response) {
             if(response.status = 403) {
                 window.location.href = '/login';
