ebb8ad091d0bc72f9ae41af053a98795c5a9b04f
Francis Perez <fcsp19191993@ufl.edu>
2015-12-06 22:14:56 -0500

    Do not merge

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -7,64 +7,15 @@ app.controller('main', function($scope) {
 
 /* Overview Controller */
 app.controller('overview', function($scope, $http) { 
-<<<<<<< HEAD
-    $scope.report = {};
-=======
     $scope.report = []; 
-<<<<<<< HEAD
-    $scope.keys = ["Clicks", "Impressions", "CTR", "CPM", "Budget USD", "Cost USD", "Start Date", "End Date", "Created Date"];
->>>>>>> 805fc47311d444675c1b71cf2938fd8ec94ce1ba
-=======
     $scope.keys = ["clicks", "impressions", "ctr", "cpm", "budget_USD", "cost_USD", "start_date", "end_date", "created_date"];
->>>>>>> 1a6332b1b2f812614bc889abc5bcf660f82d79ed
     $scope.overview_ready = false;
-    $scope.num = 0;
-    $scope.campaigns = 0;
-    $scope.costUSD = 0;
-    $scope.startDate = 0;
-    $scope.endDate = 0;
-    $scope.budgetUSD = 0;
-    $scope.impressions = 0;
-    $scope.ctr = 0;
-    $scope.cpa = 0;
-    $scope.cpm = 0;
-    $scope.cpc = 0;
-    $scope.createdDate = 0;
     
     $http.get('/api/reports?report_type=campaign').then(
         function(response) { 
-<<<<<<< HEAD
-<<<<<<< HEAD
-            var report = response.data.results;
-            console.log(report);
-
-
-
-
-            $scope.num = report.length;
-=======
-        	console.log(response.data.results[0]);
-
-			$scope.report.push(response.data.results[0].clicks);
-			$scope.report.push(response.data.results[0].impressions);
-			$scope.report.push(response.data.results[0].ctr);
-			$scope.report.push(response.data.results[0].cpm);
-			$scope.report.push(response.data.results[0].budget_USD);
-			$scope.report.push(response.data.results[0].cost);
-			$scope.report.push(response.data.results[0].start_date);
-			$scope.report.push(response.data.results[0].end_date);
-			$scope.report.push(response.data.results[0].created_date);
-
-            //$scope.report = response.data.results[0];
->>>>>>> 805fc47311d444675c1b71cf2938fd8ec94ce1ba
-            $scope.overview_ready = true;
-            $(window).resize();
-        }, 
-=======
             $scope.report = response.data.results[0];
             $scope.overview_ready = true;
         },
->>>>>>> 1a6332b1b2f812614bc889abc5bcf660f82d79ed
         function(response) {
             if(response.status = 403) {
                 window.location.href = '/login';
