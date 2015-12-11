d9aab0ef61535a2144307f1bd4826b8e0b43b543
leslierichardson95 <bookworm42795@gmail.com>
2015-12-11 00:29:13 -0500

    login comments

--- public/login/login.js
+++ public/login/login.js
@@ -1,5 +1,6 @@
 var app = angular.module('app', []);
 
+//Checks if email/password sign in combo is in the the database to proceed to main page, else return error
 app.controller('loginCtrl', function($scope, $http, $window) {
     
     $scope.show_error_message = false;
@@ -8,6 +9,7 @@ app.controller('loginCtrl', function($scope, $http, $window) {
 	$scope.password = "";
 
 	$scope.signin = function() {
+	//api call to check email and password
         $http.post('/api/Logins', {user: $scope.email, password:$scope.password})
 		.then(
 			function(response) {
